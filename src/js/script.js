bda8f5235d2a3d68ce335a1aff98aaa3631f4de7
Vitaliy <wiification@gmail.com>
2021-04-07 13:54:17 +0300

    Change tags of section from 'div' to 'section', add slider with slick-slider

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 300,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left_arrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right_arrow.png"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+      });
+});
